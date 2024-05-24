93b51de2c72d665d846f2f6c097ced09eec125fe
Shashank <codewithshash>
2024-05-24 16:48:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{textAlign: "center",color: "#9e8a76"}}>
+      <h1>I am Deployed</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Child.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Child(props) {
+  return (
+        <h1>{props.currState}</h1>
+  )
+}
+
+export default Child
