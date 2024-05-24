1260639acf073fbdbeb31df520a7719ab3e0d09c
Shashank <codewithshash>
2024-05-24 17:27:52 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from './logo.svg'
 
 function App() {
   const person = {name: "Jhon Doe",city: "Park Avanue",country: "USA"}
   return (
     <div style={{textAlign: "center",color: "#001122"}}>
       <h1>I am Deployed</h1>
+      <img src={logo} alt="logo" width={100} />
         <ul>
           <li>{person.name}</li>
           <li>{person.city}</li>
