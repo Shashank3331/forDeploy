01e421bd0e34850fa3a6416c343b51433f11abdd
Shashank <codewithshash>
2024-05-24 17:19:45 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   const person = {name: "Jhon Doe",city: "Park Avanue",country: "USA"}
   return (
-    <div style={{textAlign: "center",color: "#9e8a76"}}>
+    <div style={{textAlign: "center",color: "#001122"}}>
       <h1>I am Deployed</h1>
         <ul>
           <li>{person.name}</li>
