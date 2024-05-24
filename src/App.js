71b2386aa2035a7c20c10d9354fa9480f808fb22
Shashank <codewithshash>
2024-05-24 17:02:41 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 function App() {
+  const person = {name: "Jhon Doe",city: "Park Avanue",country: "USA"}
   return (
     <div style={{textAlign: "center",color: "#9e8a76"}}>
       <h1>I am Deployed</h1>
+        <ul>
+          <li>{person.name}</li>
+          <li>{person.city}</li>
+          <li>{person.country}</li>
+        </ul>
       <button>Check</button>
+
     </div>
   )
 }
